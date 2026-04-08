5f7479aae44aa54e12b7d37525fe01a88c40a009
Brad <bradley.johnson05@ed.cna.nl.ca>
2026-04-07 22:22:12 -0230

    Added remove item feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request, session, flash, redirect
 from flask_session import Session
 import os
 
@@ -44,11 +44,27 @@ def itemInput():
         session["items"][item_name] = {'boss_name': boss_name, 'drop_rate': drop_rate, 'kill_count': kill_count, 'img':file.filename}
 
     flash(f"{item_name} has been added!!")
-    return render_template('itemDisplay.html', items=session.get("items", {}), file_location="/static/images")
+    return render_template('itemDisplay.html', items=session["items"], file_location="/static/images")
 
 @app.route('/removeItems')
-def remove():
-    pass
+def removeItemsForm():
+    items = session.get('items', {})
+    return render_template('removeItems.html', items=items)
+
+@app.route('/removeItems', methods=['POST'])
+def removeItems():
+    items = session.get('items', {})
+    item_name = request.form.get('item_name')
+
+    if item_name and item_name in items:
+        items.pop(item_name)
+        session['items'] = items
+        session.modified = True
+        flash(f"{item_name} has been removed!!")
+    else:
+        flash('No item was removed. Please type an item from the list.')
+
+    return render_template('removeItems.html', items=items)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
